f59a4f5430820279b0bca1ec7a3bffc5d25c6ba3
omnema007 <nemaom2003@gmail.com>
2025-02-21 23:40:14 +0530

    [Om] Add. Hello from BridzeLabz

---
+++ src/main/java/com/messagingapp/MessagingApplication.java
@@ -0,0 +1,12 @@
+package com.messagingapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessagingApplication {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello From Bridgelabz";
+    }
+}
